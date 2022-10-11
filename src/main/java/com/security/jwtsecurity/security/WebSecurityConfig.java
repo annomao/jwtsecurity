7dad858534ec013cc169c45212c2f627aefa494a
annomao <omaoannkerubo@gmail.com>
2022-10-11 16:19:07 +0300

    edit security config file

--- src/main/java/com/security/jwtsecurity/security/WebSecurityConfig.java
+++ src/main/java/com/security/jwtsecurity/security/WebSecurityConfig.java
@@ -1,14 +1,50 @@
 package com.security.jwtsecurity.security;
 
 
-
+import com.security.jwtsecurity.services.AppUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AppUserDetailsService appUserDetailsService;
+    private final AuthTokenFilter authTokenFilter;
+
+    public WebSecurityConfig(AppUserDetailsService appUserDetailsService, AuthTokenFilter authTokenFilter) {
+        this.appUserDetailsService = appUserDetailsService;
+        this.authTokenFilter = authTokenFilter;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(appUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().authorizeRequests().
+                antMatchers("/auth/login/**","/auth/refresh-token").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(authTokenFilter, BasicAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().httpBasic();
+    }
+
+
 
 }
