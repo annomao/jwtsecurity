74e5634e8582f6e0818676c648bde46de7d9ab6d
annomao <omaoannkerubo@gmail.com>
2022-10-17 08:49:01 +0300

    add jwt filters

--- src/main/java/com/security/jwtsecurity/security/AuthTokenFilter.java
+++ src/main/java/com/security/jwtsecurity/security/AuthTokenFilter.java
@@ -1,7 +1,9 @@
 package com.security.jwtsecurity.security;
 
+import com.security.jwtsecurity.services.AppUserDetailsService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
@@ -23,6 +25,12 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
+    private final AppUserDetailsService appUserDetailsService;
+
+    public AuthTokenFilter(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var headerValue = request.getHeader("Authorization");
@@ -30,8 +38,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             var token = headerValue.substring(7);
             Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
             try {
-                var jwt= Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(jwt.getBody().getSubject(), null, new ArrayList<>());
+                var name= Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+                var user = appUserDetailsService.loadUserByUsername(name);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(name, null, user.getAuthorities());
                 SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
                 securityContext.setAuthentication(authToken);
                 SecurityContextHolder.setContext(securityContext);
